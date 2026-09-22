999aa2c9db4c41e7e6f6351af8dd8552b8ae0076
Mitch Ford <mfordswe@gmail.com>
2026-09-22 11:53:41 -0400

    Updates

---
+++ _next/static/chunks/app/projects/page-89d86498941c98cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[893],{3908:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,5071,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[71,79,544,358],()=>s(3908)),_N_E=e.O()}]);
---
+++ _next/static/chunks/pages/_app-408c259b59bc3e12.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1426:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(5956)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1426),n(4882))),_N_E=_.O()}]);
